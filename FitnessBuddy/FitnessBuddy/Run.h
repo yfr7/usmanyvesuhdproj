df40e7e2524b16d9c173919206bc3fd915b51175
YVES FERNANDES <YFR7@YVESs-MacBook-Pro.local>
2013-01-30 08:10:10 -0600

    add class for run and calendar image

---
+++ FitnessBuddy/FitnessBuddy/Run.h
@@ -0,0 +1,15 @@
+//
+//  Run.h
+//  FitnessBuddy
+//
+//  Created by YVES FERNANDES on 1/30/13.
+//  Copyright (c) 2013 Usman Tamanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Run : NSObject
+{
+    float distanceRunned;
+}
+@end
---
+++ FitnessBuddy/FitnessBuddy/Run.m
@@ -0,0 +1,13 @@
+//
+//  Run.m
+//  FitnessBuddy
+//
+//  Created by YVES FERNANDES on 1/30/13.
+//  Copyright (c) 2013 Usman Tamanna. All rights reserved.
+//
+
+#import "Run.h"
+
+@implementation Run
+
+@end
