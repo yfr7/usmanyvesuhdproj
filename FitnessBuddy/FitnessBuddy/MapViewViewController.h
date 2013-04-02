de2bdaac6fd741cf4b4021e29a518b1acaea906f
Usman Tamanna <usman8806@yahoo.com>
2013-04-02 17:21:26 -0500

    Added Mapview and MapViewController

---
+++ FitnessBuddy/FitnessBuddy/MapViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewViewController.h
+//  FitnessBuddy
+//
+//  Created by Usman Tamanna on 4/2/13.
+//  Copyright (c) 2013 Usman Tamanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewViewController : UIViewController
+
+@end
---
+++ FitnessBuddy/FitnessBuddy/MapViewViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewViewController.m
+//  FitnessBuddy
+//
+//  Created by Usman Tamanna on 4/2/13.
+//  Copyright (c) 2013 Usman Tamanna. All rights reserved.
+//
+
+#import "MapViewViewController.h"
+
+@interface MapViewViewController ()
+
+@end
+
+@implementation MapViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
